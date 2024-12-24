0a450ff1e9afbe34698d0f6f38f835e615cd55e5
Harsh Agarwal <Realharshagarwal@gmail.com>
2024-12-24 22:44:39 +0530

    Starting Working on Cursor Animation

---
+++ components/cursor/cursor.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+interface CursorProps{
+    color:string;
+}
+
+
+export const Cursor:FC<CursorProps>=()=>{
+    return <AnimatedCursor
+    innerSize={8}
+    outerSize={35}
+    innerScale={1}
+    outerScale={2}
+    outerAlpha={0}
+    hasBlendMode={true}
+    innerStyle={{
+      backgroundColor: 'var(--cursor-color)'
+    }}
+    outerStyle={{
+      border: '3px solid var(--cursor-color)'
+    }}
+  /> 
+}
